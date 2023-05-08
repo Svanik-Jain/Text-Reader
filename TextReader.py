f2754da16f46050939f5ec12d858524a773cdb67
Svanik-Jain <130584200+Svanik-Jain@users.noreply.github.com>
2023-05-08 15:16:45 +0530

    Update TextReader.py

--- TextReader.py
+++ TextReader.py
@@ -4,3 +4,19 @@ Word detector from github: https://github.com/githubharald/WordDetector
 
 Pytesseract
 '''
+
+
+import PyPDF2
+import pyttsx3
+
+pdfFile = PyPDF2.PdfFileReader("jemh101.pdf")
+TextToSpeech = pyttsx3.init()
+
+pageNumber = int(input("Which page do you want to read? "))
+
+Text = pdfFile.getPage(pageNumber).extract_text()
+
+line = Text.split("\n")
+
+TextToSpeech.say(f"{line[0]}")
+TextToSpeech.runAndWait()
