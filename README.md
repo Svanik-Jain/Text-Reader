5f6e7383f79e544dd3597c1b120672807023176f
AADIdevoloper <86552754+AADIdevoloper@users.noreply.github.com>
2023-05-10 13:59:57 +0530

    Added the code for repeating the words on request (#3)
    * Added code for repeating some part
    
    * Added repeatition of some part feature in code
    
    * All I have done and what's left to do
    
    First, I have told you what I have added, then I have told you the last step for our project to be submitted
    
    * Mentioned all the changes I have done in code README.md
    
    First, read what changes I have done.
    Second, read the last part of the project before submitting it.
    
    * Added the repetion feature
    
    Read README.md to understand all the changes I have done
    
    * Added the repetion feature
    
    * Added the repition feature
    
    * Added the repetition feature to the
    
    * Added the repetition feature to the code

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Text-Reader
+## Additions I have made:
+
+I have added the feature to read some part of the text if someone misses out, then read on further if some more is left out
+The code first finds the lines that the person is refering to, then reads comlete line, plus a line further.
+If the person has left out more, it asks if more is to be read.
+It also asks several times so that many parts can be read out again.
+
+Also, in the start I also added the feature to slow down the speed (because this is a text dictator.)
+
+## One last step to project completion!!!
+
+
+#### All that is left for this to put the module that converts speech to text.
+It will be used when:
+  1. which page number is to be dictated                                      --------> go to line 13 in code
+  2. what part is to be read again (variable name is "reread")                --------> go to line 39 in code
+  3. whether the part read again is to be continued (variable name is "readon")-------> go to line 47 in code
+
+All we have to do is to replace the input() function when speech to text answer.
+Rest all is done!
+
+*In case you haven't understood the code, I will properly explain everything in our next meeting*
--- TextReader.py
+++ TextReader.py
@@ -1,22 +1,63 @@
-'''Modules we can use:
+from PyPDF2 import PdfReader
 
-Word detector from github: https://github.com/githubharald/WordDetector
-
-Pytesseract
-'''
-
-
-import PyPDF2
 import pyttsx3
 
-pdfFile = PyPDF2.PdfFileReader("jemh101.pdf")
+reader = PdfReader("<<PDF_name>>.pdf")
+
+
 TextToSpeech = pyttsx3.init()
 
-pageNumber = int(input("Which page do you want to read? "))
 
-Text = pdfFile.getPage(pageNumber).extract_text()
-
-line = Text.split("\n")
-
-TextToSpeech.say(f"{line[0]}")
+TextToSpeech.say("which page number do you want?")
 TextToSpeech.runAndWait()
+page = reader.pages[int(input("which page number do you want?"))-1]
+
+
+Text = page.extract_text()
+line = Text.split("\n")
+print(Text)
+
+
+dictionary = {}
+i = 0
+for num in line:
+    i += 1
+    dictionary[i] = num
+#print(dictionary)
+    
+
+TextToSpeech.setProperty("rate", 150)  #slows down speed of dictator
+TextToSpeech.say(Text)
+TextToSpeech.runAndWait()
+
+
+reread = ''
+while reread != 'none':
+    TextToSpeech.say("Which line do you want me to repeat?")
+    TextToSpeech.runAndWait()
+    reread = input('Which line do you want me to repeat?')
+    for number,sentence in list(dictionary.items()):
+        if reread in sentence:
+            try:
+                TextToSpeech.say("I'll repeat."+"."+"."+"."+"."+"."+"."+"."+dictionary[number]+dictionary[number+1])
+                TextToSpeech.runAndWait()
+                TextToSpeech.say("Do you want me to read further?")
+                TextToSpeech.runAndWait()
+                readon =input("Do you want me to read further?")
+                #print(readon)
+                if  readon == "yes":
+                    TextToSpeech.say("I'll repeat."+"."+"."+"."+"."+"."+"."+"."+dictionary[number + 2]+dictionary[number+3])
+                    TextToSpeech.runAndWait()
+                elif readon == "no":
+                    TextToSpeech.say("Ok")
+                    TextToSpeech.runAndWait()
+                else:
+                    TextToSpeech.say("Reply in yes or no")
+                    TextToSpeech.runAndWait()
+            except:
+                TextToSpeech.say("I'll repeat."+"."+"."+"."+"."+"."+"."+"."+dictionary[number])
+                TextToSpeech.runAndWait()
+
+else:
+    TextToSpeech.say("Thank you for using Text Dictator")
+    TextToSpeech.runAndWait()
